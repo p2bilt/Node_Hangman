0111dd76c574db8916657b07b85489293aa587cc
p2bilt <peteralan@gmail.com>
2018-02-17 16:05:01 -0500

    first file outline

---
+++ index.js
@@ -0,0 +1,6 @@
+// contains the logic for the course of the game, depends on `Word.js`
+
+//   Randomly selects a word and uses the `Word` constructor to store it
+
+//   Prompts the user for each guess and keeps track of the user's remaining guesses
+
